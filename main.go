71b1c1df34b05abb249567986ced79335ef9c231
Diana Burca <elena-diana.burca@metrosystems.net>
2019-06-02 16:04:20 +0300

    Add compute endpoint

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+func Factorial(n uint64) (result uint64) {
+	if n > 0 {
+		result = n * Factorial(n-1)
+		return result
+	}
+	return 1
+}
+
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		output := map[string]string{
@@ -20,6 +28,16 @@ func main() {
 
 	http.HandleFunc("/.well-known/ready", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+	})
+
+	http.HandleFunc("/compute", func(w http.ResponseWriter, r *http.Request) {
+		result := map[uint64]uint64{}
+
+		for i := uint64(0); i < 10; i++ {
+			result[i] = Factorial(uint64(i))
+		}
+
+		json.NewEncoder(w).Encode(result)
 	})
 
 	http.ListenAndServe(":8080", nil)
